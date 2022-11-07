a33200e13a0f7a553aa70dace0dcee33bb4bb22d
stelko <stilyanmtoskov@gmail.com>
2022-11-07 23:21:08 +0000

    rewriting modal to work with next and prev btns

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 const apiUrl = 'https://randomuser.me/api/?results=12&inc=picture,name,email,location,cell,dob&nat=us';
 const gallery = document.getElementById('gallery');
 let employees = [];
+createModal();
+const modalContainer = document.querySelector('.modal-container');
 /**
  * fetches 12 random users from the randomuser api with the following info:
  * - picture
@@ -31,7 +33,7 @@ function displayEmployees(employeesData) {
                 <img class="card-img" src="${employee.picture.large}" alt="profile picture">
             </div>
             <div class="card-info-container">
-                <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+                <h3 class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
                 <p class="card-text">${employee.email}</p>
                 <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
             </div>
@@ -46,53 +48,92 @@ function displayEmployees(employeesData) {
 gallery.addEventListener('click', (e) => {
     const card = e.target.closest('.card');
     const cardIndex = card.getAttribute('data-index');
-    displayModal(cardIndex);
+    changeModal(cardIndex);
+    displayModal();
 });
 /**
- * displays a modal window on screen when a card is pressed
- * @param {number} index 
- * the modal window displays more info about the user than the card
- * can be closed by clicking on the X
+ * creates a modal and adds it after the gallery div
  */
-function displayModal(index) {
-    const {name: {first, last}, dob: {date}, cell, email, location: {city, street, state, postcode}, picture} = employees[index];
-    let birthday = new Date(date);
-    birthday = `${birthday.getMonth()}/${birthday.getDay()}/${birthday.getFullYear()}`;
-    if (document.getElementsByClassName('modal-container').length === 0) {
-        let modalHTML = `<div class="modal-container">
+function createModal() {
+    let modalHTML = `<div class="modal-container" style="display: none" data-index="">
         <div class="modal">
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
             <div class="modal-info-container">
-                <img id="img" class="modal-img" src="${picture.large}" alt="profile picture">
-                <h3 id="name" class="modal-name cap">${first} ${last}</h3>
-                <p id="email" class="modal-text">${email}</p>
-                <p id="city" class="modal-text cap">${city}</p>
+                <img id="img" class="modal-img" src="" alt="profile picture">
+                <h3 id="name" class="modal-name cap"></h3>
+                <p id="email" class="modal-text"></p>
+                <p id="city" class="modal-text cap"></p>
                 <hr>
-                <p id="cell" class="modal-text">${cell}</p>
-                <p id="address" class="modal-text">${street.number} ${street.name}, ${city}, ${state} ${postcode}</p>
-                <p id="birthday" class="modal-text">Birthday: ${birthday}</p>
+                <p id="cell" class="modal-text"></p>
+                <p id="address" class="modal-text"></p>
+                <p id="birthday" class="modal-text">Birthday: </p>
             </div>
         </div>
+        <div class="modal-btn-container">
+                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>
     </div>`;
-        gallery.insertAdjacentHTML('afterend', modalHTML);
-    } else {
-        document.getElementById('img').src = picture.large;
-        document.getElementById('name').innerHTML = `${first} ${last}`;
-        document.getElementById('email').innerHTML = email;
-        document.getElementById('city').innerHTML = city;
-        document.getElementById('cell').innerHTML = cell;
-        document.getElementById('address').innerHTML = `${street.number} ${street.name}, ${city}, ${state} ${postcode}`;
-        document.getElementById('birthday').innerHTML = `Birthday: ${birthday}`;
-    }
-    const modalContainer = document.getElementsByClassName('modal-container')[0];
-    if (modalContainer.style.display === 'none') {
-        modalContainer.style.display = 'block';
-    }
-    const modalCloseBtn = document.querySelector('#modal-close-btn');
+    gallery.insertAdjacentHTML('afterend', modalHTML);
+}
+/**
+ * changes which employee is displayed on the modal
+ * @param {number} index 
+ */
+function changeModal(index) {
+    const {name: {first, last}, dob: {date}, cell, email, location: {city, street, state, postcode}, picture} = employees[index];
+    let birthday = new Date(date);
+    modalContainer.setAttribute('data-index', index);
+    birthday = `${birthday.getMonth()}/${birthday.getDay()}/${birthday.getFullYear()}`;
+    document.getElementById('img').src = picture.large;
+    document.getElementById('name').innerHTML = `${first} ${last}`;
+    document.getElementById('email').innerHTML = email;
+    document.getElementById('city').innerHTML = city;
+    document.getElementById('cell').innerHTML = cell;
+    document.getElementById('address').innerHTML = `${street.number} ${street.name}, ${city}, ${state} ${postcode}`;
+    document.getElementById('birthday').innerHTML = `Birthday: ${birthday}`;
+}
+/**
+ * adds function to the modal close button
+ * allows users to close down the modal
+ */
+const modalCloseBtn = document.querySelector('#modal-close-btn');
     modalCloseBtn.addEventListener('click', (e) => {
         modalContainer.style.display = 'none';
-    });
+});
+/**
+ * displays the modal on the screen
+ */
+function displayModal() {
+    modalContainer.style.display = "block";
 }
+/**
+ * adds function to the prev and next buttons on the modal
+ * allows users to flick between employees
+ * without needing to close the employee and then select another one
+ */
+const modalPrev = document.getElementById('modal-prev');
+modalPrev.addEventListener('click', () => {
+    const modalIndex = parseInt(modalContainer.getAttribute('data-index'));
+    if (modalIndex === 0) {
+        changeModal(11);
+        displayModal();
+    } else {
+        changeModal(modalIndex - 1);
+        displayModal();
+    }
+});
+const modalNext = document.getElementById('modal-next');
+modalNext.addEventListener('click', () => {
+    const modalIndex = parseInt(modalContainer.getAttribute('data-index'));
+    if (modalIndex === 11) {
+        changeModal(0);
+        displayModal();
+    } else {
+        changeModal(modalIndex + 1);
+        displayModal();
+    }
+});
 /**
  * search function
  * allows users to filter out specific employees
